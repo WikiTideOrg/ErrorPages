6428f4369b80f279027f42643cb967472465e8bd
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-26 09:44:00 -0600

    MissingWiki: support WikiTide

--- MissingWiki.php
+++ MissingWiki.php
@@ -4,7 +4,18 @@
 if ( !$wgCommandLineMode ) {
 	http_response_code( 404 );
 
-	$subdomain = substr( $wgDBname, 0, -4 );
+	$centralServer = $wi->wikifarm === 'wikitide' ?
+		'meta.wikitide.com' :
+		'meta.wikiforge.net';
+
+	$logoUrl = 'https://static.wikiforge.net/metawiki/8/88/WikiForge_Logo.svg';
+	$logoAltText = $wi->wikifarm === 'wikitide' ?
+		'WikiTide Logo' :
+		'WikiForge Logo';
+
+	$requestWikiUrl = $wi->wikifarm === 'wikitide' ?
+		'https://meta.wikitide.com/wiki/Special:RequestWiki?wpsubdomain=' . substr( $wgDBname, 0, -8 ) :
+		'https://meta.wikiforge.net/wiki/Special:RequestPremiumWiki?wpsubdomain=' . substr( $wgDBname, 0, -4 );
 
 	$output = <<<EOF
 		<!DOCTYPE html>
@@ -14,10 +25,10 @@ if ( !$wgCommandLineMode ) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="Wiki Not Found" />
 				<title>Wiki Not Found</title>
-				<link rel="icon" type="image/x-icon" href="https://meta.wikiforge.net/favicon.ico" />
-				<link rel="apple-touch-icon" href="https://meta.wikiforge.net/apple-touch-icon.png" />
+				<link rel="icon" type="image/x-icon" href="https://{$centralServer}/favicon.ico" />
+				<link rel="apple-touch-icon" href="https://{$centralServer}/apple-touch-icon.png" />
 				<!-- Bootstrap core CSS -->
-				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"/>
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
 				<style>
 					/* Error Page Inline Styles */
 					body {
@@ -67,11 +78,11 @@ if ( !$wgCommandLineMode ) {
 			<div class="container">
 				<!-- Jumbotron -->
 				<div class="jumbotron">
-					<img src="https://static.wikiforge.net/metawiki/8/88/WikiForge_Logo.svg" width="130" height="130" alt="WikiForge Logo" />
+					<img src="{$logoUrl}" width="130" height="130" alt="{$logoAltText}" />
 					<h1>Wiki Not Found</h1>
 					<p class="lead">We couldn't find this wiki. Check your spelling and try again.</p>
 					<p>
-						<a href="https://meta.wikiforge.net/wiki/Special:RequestPremiumWiki?wpsubdomain={$subdomain}" class="btn btn-lg btn-outline-primary" role="button">Request the Wiki</a>
+						<a href="{$requestWikiUrl}" class="btn btn-lg btn-outline-primary" role="button">Request the Wiki</a>
 					</p>
 				</div>
 			</div>
