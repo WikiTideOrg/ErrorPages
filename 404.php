a3643432087c06c26820db6589efe08bce659e1a
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-11-01 00:55:06 -0500

    WikiForge -> WikiTide

--- 404.php
+++ 404.php
@@ -11,8 +11,8 @@ echo <<<EOF
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta name="description" content="Page not found" />
 			<title>Page not found</title>
-			<link rel="icon" type="image/x-icon" href="https://meta.wikiforge.net/favicon.ico" />
-			<link rel="apple-touch-icon" href="https://meta.wikiforge.net/apple-touch-icon.png" />
+			<link rel="icon" type="image/x-icon" href="https://meta.wikitide.org/favicon.ico" />
+			<link rel="apple-touch-icon" href="https://meta.wikitide.org/apple-touch-icon.png" />
 			<!-- Bootstrap core CSS -->
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 			<!-- Outfit font from Google Fonts -->
@@ -23,8 +23,8 @@ echo <<<EOF
 			<!-- Jumbotron -->
 			<div class="jumbotron" style="padding: 70px 0; text-align: center;">
 				<p>
-					<a href="https://wikiforge.net">
-						<img src="https://static.wikiforge.net/metawiki/8/88/WikiForge_Logo.svg" width="130" height="130" alt="WikiForge" />
+					<a href="https://wikitide.org">
+						<img src="https://static.wikiforge.net/commonswikitide/2/22/WikiTide_icon.svg" width="130" height="130" alt="WikiForge" />
 					</a>
 				</p>
 				<h1><b>Page not found</b></h1>
--- MissingWiki.php
+++ MissingWiki.php
@@ -4,25 +4,6 @@
 if ( !$wgCommandLineMode ) {
 	http_response_code( 404 );
 
-	$centralServer = $wi->wikifarm === 'wikitide' ?
-		'meta.wikitide.org' :
-		'meta.wikiforge.net';
-
-	$logoUrl = $wi->wikifarm === 'wikitide' ?
-		'https://static.wikiforge.net/commonswikitide/2/22/WikiTide_icon.svg' :
-		'https://static.wikiforge.net/metawiki/8/88/WikiForge_Logo.svg';
-	$logoAltText = $wi->wikifarm === 'wikitide' ?
-		'WikiTide' :
-		'WikiForge';
-
-	$requestWikiUrl = $wi->wikifarm === 'wikitide' ?
-		'https://meta.wikitide.org/wiki/Special:RequestWiki?wpsubdomain=' . substr( $wgDBname, 0, -8 ) :
-		'https://meta.wikiforge.net/wiki/Special:RequestPremiumWiki?wpsubdomain=' . substr( $wgDBname, 0, -4 );
-
-	$serviceMainPage = $wi->wikifarm === 'wikitide' ?
-		'https://wikitide.org' :
-		'https://wikiforge.net';
-
 	$output = <<<EOF
 		<!DOCTYPE html>
 		<html lang="en">
@@ -31,8 +12,8 @@ if ( !$wgCommandLineMode ) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="Wiki not found" />
 				<title>Wiki not found</title>
-				<link rel="icon" type="image/x-icon" href="https://{$centralServer}/favicon.ico" />
-				<link rel="apple-touch-icon" href="https://{$centralServer}/apple-touch-icon.png" />
+				<link rel="icon" type="image/x-icon" href="https://meta.wikitide.org/favicon.ico" />
+				<link rel="apple-touch-icon" href="https://meta.wikitide.org/apple-touch-icon.png" />
 				<!-- Bootstrap core CSS -->
 				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 				<!-- Outfit font from Google Fonts -->
@@ -42,18 +23,18 @@ if ( !$wgCommandLineMode ) {
 			<div class="container" style="padding: 70px 0; text-align: center;">
 				<!-- Jumbotron -->
 				<div class="jumbotron">
-					<img src="{$logoUrl}" width="130" height="130" alt="{$logoAltText}" />
+					<img src="https://static.wikiforge.net/commonswikitide/2/22/WikiTide_icon.svg" width="130" height="130" alt="WikiTide" />
 					<h1><b>Wiki not found</b></h1>
 					<p class="lead">Check your spelling and try again.</p>
 					<p>
-						<a href="{$requestWikiUrl}" class="btn btn-lg btn-outline-primary" role="button">Start this wiki</a>
+						<a href="https://meta.wikitide.org/wiki/Special:RequestWiki?wpsubdomain=" class="btn btn-lg btn-outline-primary" role="button">Start this wiki</a>
 					</p>
 				</div>
 			</div>
 			<div class="bottom-links">
 				<a href="#" onClick="history.go(-1); return false;">&larr; Go back</a>
-				<a href="{$serviceMainPage}">{$logoAltText}</a>
-				<a href="https://{$centralServer}/wiki/Special:WikiDiscover">Directory</a>
+				<a href="https://wikitide.org">WikiTide</a>
+				<a href="https://meta.wikitide.org/wiki/Special:WikiDiscover">Directory</a>
 			</div>
 		</html>
 	EOF;
